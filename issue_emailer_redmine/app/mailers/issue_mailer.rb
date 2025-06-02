ef9fe068ae3ef566dabdfd32ddcec3f8ceb68b91
eshad <uiu025@gmail.com>
2025-06-02 14:38:49 +0800

    +issue added +body fix

--- issue_emailer_redmine/app/mailers/issue_mailer.rb
+++ issue_emailer_redmine/app/mailers/issue_mailer.rb
@@ -5,9 +5,6 @@ class IssueMailer < ActionMailer::Base
     @issue = issue
     @body = body
 
-    Rails.logger.info("Sending HTML email to #{to}")
-    Rails.logger.info("Email body length: #{@body.to_s.length}")
-
     mail(to: to, subject: subject) do |format|
       format.html { render inline: @body.html_safe }
     end
